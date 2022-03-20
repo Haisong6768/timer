62a9d979be5483d24eb935dd826c10108a697d95
Haisong Qi <haisongqi6768@gmail.com>
2022-03-20 11:22:46 +1100

    Added key filter

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const text = document.getElementById("text");
+const regex = new RegExp("^[0-9]$");
 
 const onKeyPress = (e) => {
-  console.log(e.key);
+  console.log();
   text.innerText = e.key;
+  switch (e.key) {
+    case "Backspace":
+      onBackspace();
+      break;
+    case "Enter":
+      onStart();
+      break;
+    default:
+      if (regex.test(e.key)) {
+        console.log(parseInt(e.key));
+      }
+      break;
+  }
 };
 
 document.addEventListener("keydown", onKeyPress);
+
+function onClear() {
+  console.log("clear");
+}
+
+function onStart() {
+  console.log("start");
+}
+
+function onBackspace() {
+  console.log("backspace");
+}
